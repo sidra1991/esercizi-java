ffa8a35190a5f2a8148dff1aa2cde09efd9c47a7
anthony cecconi <anthony.cecconi@newtech-group.it>
2023-06-19 10:46:06 +0200

    risposte vero falso

--- Main.java
+++ Main.java
@@ -24,14 +24,50 @@ public class Main{
         System.out.println("4. Java \u00E8 un linguaggio difficile da imparare perch\u00E9 in ogni caso obbliga ad imparare \r\n" + //
                 "l'object orientation " + true);
         System.out.println("5. La Java Virtual Machine \u00E8 un software che supervisiona il software scritto in \r\n" + //
-                "Java. " + false);
+                "Java. " + true);
         System.out.println("6. La JVM gestisce la memoria automaticamente mediante la Garbage Collection. " + true);
         System.out.println("7. L'indipendenza dalla piattaforma \u00E8 una caratteristica poco importante " + false);
         System.out.println("8. Java non \u00E8 adatto per scrivere un sistema sicuro. " + false);
-        System.out.println("9. La Garbage Collection garantisce l'indipendenza dalla piattaforma. " + true);
+        System.out.println("9. La Garbage Collection garantisce l'indipendenza dalla piattaforma. " + false);
         System.out.println("10.Java \u00E8 un linguaggio gratuito che raccoglie le caratteristiche migliori di altri \r\n" + //
                 "linguaggi, e ne esclude quelle ritenute peggiori e pi\u00F9 pericolose. " + true);
-        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("---------------modulo domande l'ibro parte 2 (c)-------------");
+        System.out.println("1. La seguente dichiarazione del metodo main() è corretta:\n"+
+                                "public static main(String argomenti[]) {...}" + false);
+        System.out.println("2. La seguente dichiarazione del metodo main() \u00E8 corretta: \r\n" +
+                                "public static void Main(String args[]){...}  " + false);
+        System.out.println("3. La seguente dichiarazione del metodo main() \u00E8 corretta: \r\n" + 
+                                "public static void main(String argomenti[]) {...}  " + true);
+        System.out.println(" La seguente dichiarazione del metodo main() è corretta: "+
+                               " public static void main(String Argomenti[]) {...} "  + true);
+        System.out.println("5. La seguente dichiarazione di classe è corretta: "+
+                                "public class {...}" + false);
+        System.out.println("6. La seguente dichiarazione di classe \u00E8 corretta: \r\n" +
+                                "public Class Auto {...} " + false);
+        System.out.println("6. La seguente dichiarazione di classe \u00E8 corretta: \r\n" +
+                                "public class Auto {...}" + true);
+        System.out.println("8. E\u2019 possibile dichiarare un metodo al di fuori del blocco di codice che definisce \r\n" + //
+                                "una classe. " + false);
+        System.out.println("9. Il blocco di codice che definisce un metodo \u00E8 delimitato da due parentesi tonde. " + false);
+        System.out.println("10.Il blocco di codice che definisce un metodo è delimitato da due parentesi quadre.  " + false);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("---------------modulo domande l'ibro parte 3 (d)-------------");
+        System.out.println("1. La JVM è un software che simula un hardware" + true);
+        System.out.println("2. Il bytecode è contenuto in un file con suffisso \".class\"." + true);
+        System.out.println("3. Lo sviluppo Java consiste nello scrivere il programma, salvarlo, mandarlo in esecuzione ed infine compilarlo. " + false);
+        System.out.println("4. Lo sviluppo Java consiste nello scrivere il programma, salvarlo, compilarlo ed infine mandarlo in esecuzione. "  + true);
+        System.out.println("5. Il nome del file che contiene una classe Java deve coincidere con il nome della classe, anche se non si tiene conto delle lettere maiuscole o minuscole." + false);
+        System.out.println("6. Una volta compilato un programma scritto in Java è possibile eseguirlo su un qualsiasi sistema operativo che abbia una JVM." + true);
+        System.out.println("7. Per eseguire una qualsiasi applicazione Java basta avere un browser. " + false);
+        System.out.println("8. Il compilatore del JDK viene invocato tramite il comando \"javac\" e la JVM viene invocata tramite il comando \"java\". " + true);
+        System.out.println("9. Per mandare in esecuzione un file che si chiama \"pippo.class\", dobbiamo lanciare \r\n" + //
+                                "il seguente comando dal prompt: \"java pippo.java\".  " + false);
+        System.out.println("10.Per mandare in esecuzione un file che si chiama \"pippo.class\", dobbiamo lanciare \r\n" + //
+                                "il seguente comando dal prompt: \"java pippo.class\".   " + false);
     }
 
 
