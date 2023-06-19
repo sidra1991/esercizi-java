61a8b1189aa95a5d741f4f62dfd05403e1e2875d
anthony cecconi <anthony.cecconi@newtech-group.it>
2023-06-19 09:19:01 +0200

    esercizio 1 completo

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("---------------esercizio 1.1-------------");
+        System.out.println(" Hello, World ");
+        System.out.println("");
+        System.out.println("---------------esercizio 1.2-------------");
+        System.out.println("+---------+\n| anthony |\n+---------+");
+        System.out.println("");
+        System.out.println("---------------esercizio 1.3-------------");
+        System.out.println("");
+        System.out.println( "                      A                  \n"+
+                            "                     A A                 \n"+
+                            "                    A   A                \n"+
+                            "                   AAAAAAA               \n"+
+                            "                  A       A              \n"+
+                            "                 A         A              ");
+
+    }
+
+}
