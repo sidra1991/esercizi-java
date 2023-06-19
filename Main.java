eba5f5566e1c0e2b1498fc379a35e3e89eedeadc
anthony cecconi <anthony.cecconi@newtech-group.it>
2023-06-19 10:05:01 +0200

    risposto a domande su libro

--- Main.java
+++ Main.java
@@ -14,7 +14,27 @@ public class Main{
                             "                   AAAAAAA               \n"+
                             "                  A       A              \n"+
                             "                 A         A              ");
-
+        System.out.println("");
+        System.out.println("");
+        System.out.println("---------------modulo domande l'ibro-------------");
+        System.out.println("1. Java \u00E8 il nome di una tecnologia e contemporaneamente il nome di un linguaggio \r\n" + //
+                "di programmazione. " + true);
+        System.out.println("2. Java \u00E8 un linguaggio interpretato ma non compilato. " + false);
+        System.out.println("3. Java \u00E8 un linguaggio veloce ma non robusto. " + false);
+        System.out.println("4. Java \u00E8 un linguaggio difficile da imparare perch\u00E9 in ogni caso obbliga ad imparare \r\n" + //
+                "l'object orientation " + true);
+        System.out.println("5. La Java Virtual Machine \u00E8 un software che supervisiona il software scritto in \r\n" + //
+                "Java. " + false);
+        System.out.println("6. La JVM gestisce la memoria automaticamente mediante la Garbage Collection. " + true);
+        System.out.println("7. L'indipendenza dalla piattaforma \u00E8 una caratteristica poco importante " + false);
+        System.out.println("8. Java non \u00E8 adatto per scrivere un sistema sicuro. " + false);
+        System.out.println("9. La Garbage Collection garantisce l'indipendenza dalla piattaforma. " + true);
+        System.out.println("10.Java \u00E8 un linguaggio gratuito che raccoglie le caratteristiche migliori di altri \r\n" + //
+                "linguaggi, e ne esclude quelle ritenute peggiori e pi\u00F9 pericolose. " + true);
+        
     }
+
+
+
 
 }
